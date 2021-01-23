eb62416d6fe50e94057e0ad03cb4270c9897cf81
Ayush Yajnik <ayush.yajnik@yahoo.com>
2021-01-22 21:10:29 -0500

    database setup

---
+++ database.py
@@ -0,0 +1,25 @@
+import sqlite3
+import sqlalchemy
+import pandas as pd
+#import pyodbc
+
+
+def sql_execute(command):
+
+    a = cur.execute(command)
+    return a
+
+def connect_to_sql():
+ 
+    global con
+    global cur
+    
+    con = sqlite3.connect('walletexplorer.db')
+    cur = con.cursor()
+
+df = pd.read_csv("D:\\searchengine-covid\\article_info.csv")
+df.to_sql('Article_info_new', con = con)
+
+
+
+
